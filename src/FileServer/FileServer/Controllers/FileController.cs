e7097ec1473a9e7140bcdd90e54cb92562312fd0
moerwald <andreas.mewald@gmail.com>
2020-11-04 19:27:53 +0100

    Cleanup

--- src/FileServer/FileServer/Controllers/FileController.cs
+++ src/FileServer/FileServer/Controllers/FileController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FileServer.Controllers
 {
@@ -13,39 +15,20 @@ namespace FileServer.Controllers
         private readonly ILogger<FileController> _logger;
         private readonly IProvideSoftware _provideSoftware;
 
-        public FileController(ILogger<FileController> logger, IProvideSoftware provideSoftware)
+        public FileController(ILogger<FileController> logger, 
+                              IProvideSoftware provideSoftware
+                              )
         {
             _logger = logger;
             _provideSoftware = provideSoftware;
         }
 
-        //[HttpGet("download-file/{fileId}")]
-        //public IActionResult DownloadFile(int fileId)
-        //{
-        //    var filePath = @"C:\Users\andre\source\repos\WebAppIdentity\WebAppIdentity\Startup.cs";
-        //    if (filePath == null) return NotFound();
-
-        //    return PhysicalFile(filePath, MimeTypes.GetMimeType(filePath), Path.GetFileName(filePath));
-        //}
-
-        //[HttpGet("download-file/{fileId}")]
-        [HttpGet("latest-version/{prefix}")]
-        public async Task<IActionResult> DownloadFile(string prefix)
+        [HttpGet("latest-version")]
+        public async Task<IActionResult> DownloadFile(CancellationToken token)
         {
-            // Since this is just and example, I am using a local file located inside wwwroot
-            // Afterwards file is converted into a stream
-            //var path = Path.Combine(_hostingEnvironment.WebRootPath, "Sample.xlsx");
-            //var path = @"C:\Users\andre\source\repos\WebAppIdentity\WebAppIdentity\Startup.cs";
-
-
-            var file = await _provideSoftware.GetPathForLatestSoftwareAsync("jjj");
-
-            var path = @"C:\Users\andre\Downloads\LINQPad6-Beta.zip";
-            var fs = new FileStream(path, FileMode.Open);
-
-            // Return the file. A byte array can also be used instead of a stream
-            return File(fs, "application /octet-stream", "LinqPad.zip"); 
+            var file = await _provideSoftware.GetPathForLatestSoftwareAsync(token);
+            var fs = new FileStream(file, FileMode.Open);
+            return File(fs, "application /octet-stream", Path.GetFileName(file));
         }
-
     }
 }
