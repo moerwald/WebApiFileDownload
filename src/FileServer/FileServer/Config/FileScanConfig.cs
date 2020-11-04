a9009ff5e5dcda9d77b202a02dc05628c65fd969
moerwald <andreas.mewald@gmail.com>
2020-11-04 19:27:35 +0100

    Config object

---
+++ src/FileServer/FileServer/Config/FileScanConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileServer.Config
+{
+    public class FileScanConfig
+    {
+        public IEnumerable<string> Directories { get; set; }
+        public string Filter { get; set; }
+        public string CaptureGroupName { get; set; }
+    }
+}
