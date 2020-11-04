409c19e7f34170be7a3b2887440519ffb3d8308f
moerwald <andreas.mewald@gmail.com>
2020-11-04 19:28:11 +0100

    Registered dependencies

--- src/FileServer/FileServer/Startup.cs
+++ src/FileServer/FileServer/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FileServer.Config;
+using FileServer.Services.SoftwareProvider;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,13 @@ namespace FileServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Options
+            services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
+            services.Configure<FileScanConfig>(Configuration.GetSection(nameof(FileScanConfig)));
+
+            // Dependencies
+            _ = services.AddTransient<IProvideSoftware, GetHighestSoftwareVersion>();
+            _ = services.AddTransient<IProvideFilesInDirectory, ProvideFilesInDirectory>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
